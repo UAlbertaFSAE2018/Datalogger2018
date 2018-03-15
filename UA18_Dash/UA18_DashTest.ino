203c1aef255bafcdf7f9f9f9bcbc9b41381fc287
fdrury <fdrury@ualberta.ca>
2018-03-15 16:42:20 -0600

    Added dash code

---
+++ UA18_Dash/UA18_DashTest.ino
@@ -0,0 +1,36 @@
+#include "dash.h"
+
+#define DASH_PIN    6
+
+Dash dash = Dash(DASH_PIN);
+
+void setup() {
+    dash.begin();
+}
+
+void loop() {
+    dash.setGear(0);
+    dash.setTach(2000);
+    dash.show();
+    delay(5000);
+    
+    for(int i = 1; i < 6; i++){
+        dash.setGear(i);
+        for(int j = 2000; j <= MAX_RPM; j += 100){
+            dash.setTach(j);
+            dash.show();
+            delay(10);
+        }
+    }
+    
+    delay(500);
+    
+    for(int i = 5; i > 0; i--){
+        dash.setGear(i);
+        for(int i = MAX_RPM; i >= 2000; i -= 100){
+            dash.setTach(i);
+            dash.show();
+            delay(10);
+        }
+    }
+}
