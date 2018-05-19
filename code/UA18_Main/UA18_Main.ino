85fd08a34518d5133e8524a5f41cc1c4a113a50f
fdrury <fdrury@ualberta.ca>
2018-05-19 14:15:13 -0600

    added dash and sensorshield to main

--- code/UA18_Main/UA18_Main.ino
+++ code/UA18_Main/UA18_Main.ino
@@ -1,17 +1,42 @@
-#define DEBUG true
-#define NUM_VALUES 50
+#include "dash.h"
+#include "sensorShield.h"
+
+#define DEBUG_MODE true
+#define NUM_VALUES 32
+#define DASH_PIN 6
+
+SensorShield sensorShield = SensorShield();
+Dash dash = Dash(DASH_PIN);
 
 uint32_t values[NUM_VALUES] = {};
 
 void setup() {
     Serial.begin(9600);
+    dash.begin();
+    if(DEBUG_MODE){
+        dash.test();
+    }
+    dash.clear();
 }
 
 void loop() {
-    updateGPS();
+    int *sensorValues = sensorShield.getSensorValues();
+    if(DEBUG_MODE){
+        for(int i = 0; i < NUM_VALUES; i++){
+            Serial.print(sensorValues[i]);
+            Serial.print(" ");
+        }
+        Serial.println();
+    }
+
+    // TODO: add real values and temp warning lights
+    dash.setGear(0);
+    dash.setTach(2000);
+    dash.show();
+    
+    /*updateGPS();
     updateOrientation();
     updateCAN();
-    updateSensors();
     updateDash();
-    writeSD();
+    writeSD();*/
 }
