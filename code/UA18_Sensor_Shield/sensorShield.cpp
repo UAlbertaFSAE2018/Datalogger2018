8b11442dfdc65b779b465abfa7d80651644cbe65
fdrury <fdrury@ualberta.ca>
2018-05-19 13:54:42 -0600

    completed basic layout of SensorShield class

--- code/UA18_Sensor_Shield/sensorShield.cpp
+++ code/UA18_Sensor_Shield/sensorShield.cpp
@@ -1,152 +1,24 @@
-#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
 #include "sensorShield.h"
 
-//based on Lightdot 8x8 font
-static const bool gearNumbers[6][64] = {{
-    1, 1, 1, 0, 0, 1, 1, 1,
-    1, 1, 1, 1, 0, 1, 1, 1,
-    1, 1, 1, 1, 0, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 0, 1, 1, 1, 1,
-    1, 1, 1, 0, 0, 1, 1, 1},{ /* N */
-    
-    0, 0, 0, 1, 1, 1, 0, 0,
-    0, 0, 1, 1, 1, 1, 0, 0,
-    0, 1, 1, 1, 1, 1, 0, 0,
-    0, 0, 0, 1, 1, 1, 0, 0,
-    0, 0, 0, 1, 1, 1, 0, 0,
-    0, 0, 0, 1, 1, 1, 0, 0,
-    0, 0, 0, 1, 1, 1, 0, 0,
-    0, 0, 0, 1, 1, 1, 0, 0},{ /* 1 */
-    
-    0, 1, 1, 1, 1, 1, 1, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 0, 0, 1, 1, 1,
-    0, 0, 0, 0, 1, 1, 1, 0,
-    0, 0, 1, 1, 1, 0, 0, 0,
-    1, 1, 1, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1},{ /* 2 */
-    
-    1, 1, 1, 1, 1, 1, 1, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 0, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 0,
-    1, 1, 1, 1, 1, 1, 1, 0,
-    0, 0, 0, 0, 0, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 0},{ /* 3 */
-    
-    1, 1, 1, 0, 1, 1, 1, 0,
-    1, 1, 1, 0, 1, 1, 1, 0,
-    1, 1, 1, 0, 1, 1, 1, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 1, 1, 1, 0,
-    0, 0, 0, 0, 1, 1, 1, 0,
-    0, 0, 0, 0, 1, 1, 1, 0},{ /* 4 */
-    
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 0, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    0, 1, 1, 1, 1, 1, 1, 0} /* 5 */
-};
 
-SensorShield::SensorShield(uint8_t pin){
-    strip = Adafruit_NeoPixel(NUM_PIXELS, pin, NEO_GRB + NEO_KHZ800);
-    
-    gearColor = strip.Color(LED_OFF, LED_OFF, LED_ON);
-    tempColor = strip.Color(LED_ON, LED_OFF, LED_OFF);
-    
-    tachColor[0] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[1] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[2] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[3] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[4] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[5] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[6] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[7] = strip.Color(LED_OFF, LED_ON, LED_OFF);
-    tachColor[8] = strip.Color(LED_ON, LED_ON, LED_OFF);
-    tachColor[9] = strip.Color(LED_ON, LED_ON, LED_OFF);
-    tachColor[10] = strip.Color(LED_OFF, LED_OFF, LED_ON);
-    tachColor[11] = strip.Color(LED_ON, LED_OFF, LED_OFF);
+SensorShield::SensorShield(){
+    pinMode(SELECT_PIN0, OUTPUT);
+    pinMode(SELECT_PIN1, OUTPUT);
+    pinMode(SELECT_PIN2, OUTPUT);
+    pinMode(SELECT_PIN3, OUTPUT);
 }
 
-void SensorShield::begin(void){
-    strip.begin();
-}
+int SensorShield::getSensorValues(){
+    for(int i = 0; i < 0b1111; i++){
+        digitalWrite(SELECT_PIN0, i & 0b0001);
+        digitalWrite(SELECT_PIN1, i & 0b0010);
+        digitalWrite(SELECT_PIN2, i & 0b0100);
+        digitalWrite(SELECT_PIN3, i & 0b1000);
 
-void SensorShield::setBrightness(uint8_t b){
-    
-}
-
-void SensorShield::test(void){
-    for(uint8_t i = 0; i < NUM_PIXELS; i++){
-        strip.setPixelColor(i, LED_ON, LED_OFF, LED_OFF);
-    }
-    strip.show();
-    delay(1000);
-    for(uint8_t i = 0; i < NUM_PIXELS; i++){
-        strip.setPixelColor(i, LED_OFF, LED_ON, LED_OFF);
-    }
-    strip.show();
-    delay(1000);
-    for(uint8_t i = 0; i < NUM_PIXELS; i++){
-        strip.setPixelColor(i, LED_OFF, LED_OFF, LED_ON);
-    }
-    strip.show();
-    delay(1000);
-}
-
-void SensorShield::show(void){
-    strip.show();
-}
-
-void SensorShield::clear(void){
-    strip.clear();
-}
-
-void SensorShield::setTempLight(uint8_t light){
-    strip.setPixelColor(light, tempColor);
-}
-
-void SensorShield::clearTempLight(uint8_t light){
-    strip.setPixelColor(light, 0);
-}
-
-void SensorShield::setTach(uint16_t rpm){
-    clearTach();
-    for(uint8_t i = TACH_START; i - TACH_START < rpm / (MAX_RPM / (TACH_LENGTH + 1)); i++){
-        if(i >= TACH_START + TACH_LENGTH){
-            break;
+        for(int j = 0; j < 16; j++){
+            
         }
-        strip.setPixelColor(i, tachColor[i]);
-    }
-}
-
-void SensorShield::clearTach(void){
-    for(uint8_t i = TACH_START; i < TACH_START + TACH_LENGTH; i++){
-        strip.setPixelColor(i, 0);
-    }
-}
-
-void SensorShield::setGear(uint8_t gear){
-    clearGear();
-    for(uint8_t i = GEAR_START; i < GEAR_START + GEAR_LENGTH; i++){
-        strip.setPixelColor(i, gearColor * gearNumbers[gear][i - GEAR_START]);
-    }
-}
-
-void SensorShield::clearGear(void){
-    for(uint8_t i = GEAR_START; i < GEAR_START + GEAR_LENGTH; i++){
-        strip.setPixelColor(i, 0);
     }
 }
 
--- code/UA18_Sensor_Shield/sensorShield.h
+++ code/UA18_Sensor_Shield/sensorShield.h
@@ -1,45 +1,27 @@
-#ifndef DASH_H
-#define DASH_H
+#ifndef SENSORSHIELD_H
+#define SENSORSHIELD_H
 
-#include <Adafruit_NeoPixel.h>
 
-#define NUM_PIXELS  78
-#define LED_OFF     0
-#define LED_ON      255
+#define SELECT_PIN0     7
+#define SELECT_PIN1     4
+#define SELECT_PIN2     10
+#define SELECT_PIN3     8
 
-#define COOLANT_TEMP_LIGHT  77
-#define OIL_TEMP_LIGHT      78
-#define TACH_START          0
-#define TACH_LENGTH         12
-#define GEAR_START          12
-#define GEAR_LENGTH         64
+#define ANALOG0         A1
+#define ANALOG1         A2
 
-#define MAX_RPM     12000
+#define MUX_SIZE        16
 
-//TODO: add color definitions
 
 class SensorShield {
     public:
-        Dash(uint8_t pin);
+        SensorShield();
         
-        void begin();
-        void setBrightness(uint8_t b);
-        void test(void);
-        void show(void);
-        void clear(void);
-
-        void setTempLight(uint8_t light);
-        void clearTempLight(uint8_t light);
-        void setTach(uint16_t rpm);
-        void clearTach(void);
-        void setGear(uint8_t gear);
-        void clearGear(void);
+        int *getSensorValues();
     protected:
-        Adafruit_NeoPixel strip;
-        uint8_t stripPin;
-        uint32_t gearColor;
-        uint32_t tempColor;
-        uint32_t tachColor[TACH_LENGTH];
+        // TODO: account for NC pins
+        int sensorValues[MUX_SIZE * 2];
 };
+
 
 #endif
